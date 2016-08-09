2c8d3a769b7d5965ca97701f7ae977d300e6cb66
Minh Nhat <minhnhat1692@gmail.com>
2016-08-09 14:33:34 +0700

    Modified error message. Sign up function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
-  def new
+  def show
+  	@user = User.find(params[:id])
+  	debugger
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Welcome to Helia"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end	
 end
