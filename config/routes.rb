ceee0a49ff38815d6c9886cd19e7436ee449cc16
Minh Nhat <minhnhat1692@gmail.com>
2016-08-09 14:51:46 +0700

    Add homepage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'home/new'
 
+  get 'users/new'
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
