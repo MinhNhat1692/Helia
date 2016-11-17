1dab7e8a08598648cbe783c4482cb95f7f15f455
tranducck <ducck92@gmail.com>
2016-11-17 17:32:22 +0700

    Add associate with station_id for summary and pool API

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -13,19 +13,19 @@ class PollsController < ApplicationController
         max = 10
         while !flag && round <= max do
           ActiveRecord::Base.connection.clear_query_cache
-          h["bill_info"] = BillInfo.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["customer_record"] = CustomerRecord.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["employee"] = Employee.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["medicine_bill_in"] = MedicineBillIn.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["medicine_prescript_external"] = MedicinePrescriptExternal.where("created_at >= ? OR updated_at >= ?", 
+          h["bill_info"] = BillInfo.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["customer_record"] = CustomerRecord.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["employee"] = Employee.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_bill_in"] = MedicineBillIn.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_prescript_external"] = MedicinePrescriptExternal.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", 
                                                                              t, t)
-          h["medicine_prescript_internal"] = MedicinePrescriptInternal.where("created_at >= ? OR updated_at >= ?", 
+          h["medicine_prescript_internal"] = MedicinePrescriptInternal.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", 
                                                                              t, t)
-          h["medicine_price"] = MedicinePrice.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["medicine_sample"] = MedicineSample.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["medicine_supplier"] = MedicineSupplier.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["position"] = Position.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["room"] = Room.where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_price"] = MedicinePrice.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_sample"] = MedicineSample.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_supplier"] = MedicineSupplier.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["position"] = Position.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["room"] = Room.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
           flag = h["bill_info"].present? || h["customer_record"].present? || h["employee"].present? || h["medicine_bill_in"].present? || h["medicine_prescript_external"].present? || h["medicine_prescript_internal"].present? || h["medicine_price"].present? || h["medicine_sample"].present? || h["medicine_supplier"].present? || h["position"].present? || h["room"].present?
           sleep 10
           round += 1
@@ -44,19 +44,19 @@ class PollsController < ApplicationController
         max = 10
         while !flag && round <= max do
           ActiveRecord::Base.connection.clear_query_cache
-          h["bill_info"] = BillInfo.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["customer_record"] = CustomerRecord.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["employee"] = Employee.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["medicine_bill_in"] = MedicineBillIn.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["medicine_prescript_external"] = MedicinePrescriptExternal.where("created_at >= ? OR updated_at >= ?", 
+          h["bill_info"] = BillInfo.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["customer_record"] = CustomerRecord.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["employee"] = Employee.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_bill_in"] = MedicineBillIn.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_prescript_external"] = MedicinePrescriptExternal.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", 
                                                                              t, t)
-          h["medicine_prescript_internal"] = MedicinePrescriptInternal.where("created_at >= ? OR updated_at >= ?", 
+          h["medicine_prescript_internal"] = MedicinePrescriptInternal.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", 
                                                                              t, t)
-          h["medicine_price"] = MedicinePrice.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["medicine_sample"] = MedicineSample.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["medicine_supplier"] = MedicineSupplier.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["position"] = Position.where("created_at >= ? OR updated_at >= ?", t, t)
-          h["room"] = Room.where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_price"] = MedicinePrice.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_sample"] = MedicineSample.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["medicine_supplier"] = MedicineSupplier.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["position"] = Position.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
+          h["room"] = Room.where(station_id: @station.id).where("created_at >= ? OR updated_at >= ?", t, t)
           flag = h["bill_info"].present? || h["customer_record"].present? || h["employee"].present? || h["medicine_bill_in"].present? || h["medicine_prescript_external"].present? || h["medicine_prescript_internal"].present? || h["medicine_price"].present? || h["medicine_sample"].present? || h["medicine_supplier"].present? || h["position"].present? || h["room"].present?
           sleep 10
           round += 1
